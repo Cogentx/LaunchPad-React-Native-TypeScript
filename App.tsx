169b2f38d8857e094dc1852062e994afa1eca153
Cogentx <code@cogentx.com>
2022-04-17 20:58:41 -0230

    Setup Stack Navigator with initial two screens

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
-import Home from './components/Home';
+import Home from './src/screens/HomeScreen';
 import React from 'react';
 import { TailwindProvider } from 'tailwind-rn';
 import utilities from './tailwind.json';
+import { NavigationContainer } from '@react-navigation/native';
+import StackNavigator from './StackNavigator';
 
 export default function App() {
   return (
     <TailwindProvider utilities={utilities}>
-      <Home />
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
     </TailwindProvider>
   );
 }
