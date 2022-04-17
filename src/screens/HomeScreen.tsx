f75407add2e7b0967aeb36ef8f3d78d4d2c34fb9
Cogentx <code@cogentx.com>
2022-04-17 21:06:42 -0230

    Navigation between screens working

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,16 +1,16 @@
-import { Button, Text, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import { useTailwind } from 'tailwind-rn';
 import React from 'react';
+import { Button, Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useTailwind } from 'tailwind-rn';
 
 export default function HomeScreen() {
   const tw = useTailwind();
+  const navigation = useNavigation();
 
   return (
     <View style={tw('flex-1 justify-center items-center')}>
       <Text>Legato Capture</Text>
-      <Button title="Capture" />
-      <StatusBar style="auto" />
+      <Button title="Chat Now" onPress={() => navigation.navigate('Chat')} />
     </View>
   );
 }
